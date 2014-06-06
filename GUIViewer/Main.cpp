37932984c6f07891ee29693a0033ab3edeba03c5
Edward Liu <shiqiu1105@gmail.com>
2014-06-05 18:52:16 -0700

    C++ delegate in progress.

--- GUIViewer/Main.cpp
+++ GUIViewer/Main.cpp
@@ -1,5 +1,29 @@
 #include <Windows.h>
 
+class Object
+{
+public:
+	virtual ~Object()
+	{
+	}
+};
+
+template<class Class, typename... Params>
+class Delegate : public Object
+{
+	typedef void (Class::*FuncHandler) (Params...);
+private:
+	Class* mpListener;
+
+public:
+	void Invoke(Params... params)
+	{
+		(mpOwner->*FuncHandler)(params...)
+	}
+
+	const Class* GetListener() { return mpListener; }
+};
+
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR cmdArgs, int cmdShow)
 {
 	return 0;
