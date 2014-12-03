12fa2ba479827de610946657fd31de6d1dd8e863
Edward Liu <shiqiu1105@gmail.com>
2014-12-02 16:45:06 -0800

    Basic operators added to Vec class.

--- EDXUtil/Math/VecBase.h
+++ EDXUtil/Math/VecBase.h
@@ -8,10 +8,25 @@ namespace EDX
 	public:
 		T v[N];
 
-		Vec(const T val)
+		Vec()
+		{
+		}
+		__forceinline Vec(const T val)
 		{
 			for (auto i = 0; i < N; i++)
 				v[i] = val;
+		}
+		__forceinline Vec(const Vec& vCopyFrom)
+		{
+			for (auto i = 0; i < N; i++)
+				v[i] = vCopyFrom[i];
+		}
+		template<class T1>
+		__forceinline Vec& operator = (const Vec<N, T1>& vOther)
+		{
+			for (auto i = 0; i < N; i++)
+				v[i] = T(vOther[i]);
+			return *this;
 		}
 
 		__forceinline const T& operator [] (const size_t idx) const { return v[idx]; }
